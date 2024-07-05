9420813908f142ee840ab1f12f20c9bbfdcd5db4
tg0xff <tg0xff@proton.me>
2024-07-05 17:47:41 -0600

    Define validateEmail()

--- main.js
+++ main.js
@@ -2,4 +2,13 @@ class Validate {
   constructor() {
     this.form = document.querySelector("form");
   }
+  validateEmail() {
+    const email = this.form.querySelector("#email");
+    const output = this.form.querySelector('output[for="email"]');
+    if (email.validity.valueMissing) {
+      this.showError(output, "The email field is empty.");
+    } else if (email.validity.typeMismatch) {
+      this.showError(output, "Please enter a valid email address.")
+    }
+  }
 }
